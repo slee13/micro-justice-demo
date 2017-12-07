cc8e7ff59e92f141560ea7dbf5edb0b696d0978f
Stephanie Lee <steph@Stephanies-MacBook-Pro.local>
2017-12-07 03:29:20 +0100

    add styles, add firebase connection

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# micro-justice-demo
+# Micro Justice PENTA Demo
+This will be displayed on desktop views
+The mobile view holds the voting interaction.  (Currently lives as a framer prototype here: https://framer.cloud/MlOjc _credit to Kelvyn for help with initial prototype_)
+
+
+The desktop view shows the data visualization (Thank you to Oliver McCormack for initial code base and all trigonometry):
+1. Display all recorded votes on all instances of microaggressions (MA) _currently shows all recorded votes on one instance of a microaggression_
+2. Display a new recorded vote on an instance of MA
+3. Display votes for an instance of a MA when it is selected
+4. Show related list of micro justices for that MA if selected
+
+Nice to have:
+1. Animate tokens to bundle together when a MA is selected
+2. Show instructions on how to use with phone
--- src/main.js
+++ src/main.js
@@ -1,14 +1,125 @@
-var RADIUS = 20;
-var SPACING = 1;
+// Set the configuration for your app
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBRCaeKgH9VUtqeDLCHSIlNZUhJ0_GaDqU",
+  authDomain: "microjustice-idp.firebaseapp.com",
+  databaseURL: "https://microjustice-idp.firebaseio.com",
+  projectId: "microjustice-idp",
+  storageBucket: "microjustice-idp.appspot.com",
+  messagingSenderId: "1031959043789"
+};
+
+//heh
+var ready = false;
+//configurations for shapes
+var RADIUS = 15;
+var SPACING = 3;
 var COLORS = {
-    hexagon:'#ff7077',
-    chevron: '#ffd851',
-    triangle: '#a5ecfe'
+    hexagon:'#F0574A',
+    chevron: '#F9E600',
+    triangle: '#B6FCFF'
 }
+var velocity = {x:5, y:5};
+disturbance = .03;
+
+//setting background color of canvas
+var rect = new Path.Rectangle({
+    point: [0, 0],
+    size: [view.size.width, view.size.height],
+    selected: true
+});
+rect.sendToBack();
+rect.fillColor = '#EFBBB6';
 
 // objs: global object holding all visible shapes that have
 // been appended to the map.
 window.objs = [];
+
+//votes contains the arrays which hold each microaggression and their related votes
+var hexCount = 0;
+var chevCount = 0;
+var triCount = 0;
+
+// define initial state here
+window.configs = {
+    active: 'MA0',
+    items: {
+        // each key like 'abc-id' is some sort of "group" or "issue" id
+        // init: just the initial state you should set it with
+        'MA0': {init: ['hexagon', 'hexagon','hexagon', 'hexagon','hexagon', 'hexagon']},
+        'MA1': {init: ['chevron', 'chevron']},
+        'MA2': {init: ['triangle', 'triangle', 'triangle']}
+    }
+}
+
+//init firebase to get all individual logged votes
+firebase.initializeApp(config);
+var getVotes = firebase.database().ref('answers/');
+
+//get all data upfront
+getVotes.on('value', gotData, errData);
+//get a snapshot of the most recently added
+getVotes.on('child_added', newData, errData);
+
+function gotData(data){
+  if (ready){
+   return;
+ } else {
+  votes = data.val();
+  console.log(votes);
+  for (var vote in votes) {
+    var v = votes[vote];
+    var sh = v.optionId;
+    var ma = v.questionId;
+    console.log(ma);
+
+   if(!window.configs.items[ma]){
+     window.configs.items[ma] = {};
+      window.configs.items[ma].init = [];
+    } else {
+       window.configs.items[ma].init.push(sh);
+    }
+    console.log(configs);
+  }
+  //when finished getting data, call main
+  main();
+}
+}
+
+//looking at the latest added shape
+function newData(snapshot,prevChildKey) {
+
+  if(ready) {
+      var newPost = snapshot.val();
+      var newShape = newPost.optionId;
+      var questionID = newPost.questionId;
+      configs.active = questionID;
+      addShape(configs.items[questionID].objs, newShape)
+
+
+      if (newShape == "hexagon") {
+        hexCount++;
+      }
+      if (newShape == "chevron") {
+        chevCount++;
+      }
+      if (newShape == "triangle") {
+        triCount++;
+      }
+    } else {
+      return;
+    }
+    // console.log(newPost);
+    // console.log(newShape);
+  // console.log(hexCount);
+  // console.log(chevCount);
+  // console.log(triCount);
+}
+function errData(err){
+  console.log("Error!");
+  console.log(err);
+}
+
 
 /**
  * Given all currently placed items, find valid positions for a new item.
@@ -18,7 +129,7 @@ window.objs = [];
  *
  * Returns an array of valid (invisible) shape objects.
  */
-var findCandidate = function(objs, nextShapeType, defaultCenter) {
+window.findCandidate = function(objs, nextShapeType, defaultCenter) {
 
     var target = objs[randint(0,objs.length)];
     var center = target ? target._center : defaultCenter;
@@ -43,38 +154,63 @@ var findCandidate = function(objs, nextShapeType, defaultCenter) {
  * `objs` is an array of currently placed objects. most likely the global `objs`
  * `type` is a string, one of ['hexagon', 'triangle', 'chevron']
  */
-var addShape = function(objs, type, defaultCenter) {
+// var addShapeToClumpArray = function(objs, type, defaultCenter) {
+//     // get valid new positions
+//     var newShape = findCandidate(objs, type, defaultCenter);
+//     // assign a target to the object to animate toward.
+//     // superhack: lib.intersects checks for the _target to 'reserve'
+//     // the position, even if an animated object isn't there yet.
+//     newShape._target = newShape.clone();
+//     // newShape.fillColor = COLORS[newShape._type];
+//     // newShape.opacity = 0.1;
+//     // newShape.position = new Point(randint(0, view.viewSize.width), randint(0, view.viewSize.height));
+//     objs.push(newShape);
+// };
+
+window.addShape = function(objs, type, defaultCenter) {
+
     // get valid new positions
     var newShape = findCandidate(objs, type, defaultCenter);
     // assign a target to the object to animate toward.
     // superhack: lib.intersects checks for the _target to 'reserve'
     // the position, even if an animated object isn't there yet.
+    console.log(newShape);
     newShape._target = newShape.clone();
     newShape.fillColor = COLORS[newShape._type];
     newShape.opacity = 0.1;
+    newShape.strokeWidth = 2;
+    newShape.strokeColor = '#231F20';
     newShape.position = new Point(randint(0, view.viewSize.width), randint(0, view.viewSize.height));
+    newShape.velocity = {x:Math.random() * velocity.x, y:Math.random() * velocity.y};
     objs.push(newShape);
+
+    // add to all shapes adds
 };
+
+
 
 // simple addition of shapes to the page
 document.getElementById('chevron').addEventListener('click', function() {
-    addShape(objs, 'chevron', view.center);
+    configs.active = 'MA0';
 });
 
 document.getElementById('hexagon').addEventListener('click', function() {
-    addShape(objs, 'hexagon', view.center);
+    configs.active = 'MA1';
 });
 
 document.getElementById('triangle').addEventListener('click', function() {
-    addShape(objs, 'triangle', view.center)
+    configs.active = 'MA2';
 });
 
 document.getElementById('auto').addEventListener('click', function() {
-    for (var i=0;i<200;i++) {
+    for (var i=0;i<hexCount;i++) {
         window.setTimeout(function() {
-            var shapes = ['hexagon', 'chevron', 'triangle'];
-            var randomShape = shapes[randint(0, shapes.length)];
-            addShape(objs, randomShape, view.center);
+            addShape(objs, 'hexagon', view.center);
+        }, 50*i);
+    }
+    for (var i=0;i<chevCount;i++) {
+        window.setTimeout(function() {
+            addShape(objs2, 'chevron', view.center);
         }, 50*i);
     }
 });
@@ -86,21 +222,48 @@ document.getElementById('auto').addEventListener('click', function() {
  * properties toward _target (if it exists);
  */
 function onFrame(event) {
+
+  // don't start drawing until main() has been called
+  if (ready){
     var freq = 10;
-    for (var i=0;i<objs.length;i++) {
-        var obj = objs[i];
-        if (obj._target) {
-            var vec = obj._target.position - obj.position;
-            var rotationDelta = Math.round(obj._target._rotation - obj.rotation);
-            var opacityDiff = obj._target.opacity - obj.opacity;
-            obj.position += vec / freq;
-            // super hack!
-            if (rotationDelta < 0.000001) {
-                obj.rotate(rotationDelta / freq);
+
+    var activeKey = configs.active;
+    for (var key in configs.items) {
+        var o = configs.items[key];
+        for (var i=0;i<o.objs.length;i++) {
+            var obj = o.objs[i];
+            if (key == activeKey) {
+                if (obj._target) {
+                    var vec = obj._target.position - obj.position;
+                    var rotationDelta = Math.round(obj._target._rotation - obj.rotation);
+                    var opacityDiff = obj._target.opacity - obj.opacity;
+                    obj.position += vec / freq;
+                    // super hack!
+                    if (rotationDelta < 0.000001) {
+                        obj.rotate(rotationDelta / freq);
+                    }
+                    obj.opacity = obj.opacity + (opacityDiff / freq);
+                }
+            } else {
+                if (obj.position.x > view.size.width || obj.position.x < 0) {
+                  obj.velocity.x *= -1;
+                }
+
+                if ( obj.position.y > view.size.height || obj.position.y < 0) {
+                  obj.velocity.y *= -1;
+                }
+                var opacityDiff = obj._target.opacity - obj.opacity;
+                obj.position.x += Math.sin(obj.rotation) * obj.velocity.x;
+                obj.position.y -= Math.cos(obj.rotation) * obj.velocity.y;
+                obj.opacity = obj.opacity + (opacityDiff / freq);
             }
-            obj.opacity = obj.opacity + (opacityDiff / freq);
         }
     }
+  } else {
+    return;
+  }
+    // animating
+    // if clumping, don't animate
 }
 
 
@@ -109,9 +272,15 @@ var main = function() {
     // add an initial object
     // default center (paperjs provides `view`)
     var center = view.center;
-    var shapes = ['hexagon', 'chevron', 'triangle'];
-    var randomShape = shapes[randint(0, shapes.length)];
-    addShape(objs, randomShape, center);
+    for (var key in configs.items) {
+        var o = configs.items[key];
+        o.objs = [];
+        _.each(o.init, function(shapeType) {
+            addShape(o.objs, shapeType, center);
+        });
+    }
+    ready = true;
+    console.log('now populating shape');
 }
 
-main();
+//main();
